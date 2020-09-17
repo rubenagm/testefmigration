c407bee1b047a816d71b6061e64ee8012e8444f9
Ruben Guardado <rguardado@viqsolutions.com>
2020-09-17 13:46:56 -0500

    adding empty migration

---
+++ TestEF.EntityFramework/Migrations/202009171846136_M2_Empty_Add.cs
@@ -0,0 +1,16 @@
+﻿namespace TestEF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class M2_Empty_Add : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
