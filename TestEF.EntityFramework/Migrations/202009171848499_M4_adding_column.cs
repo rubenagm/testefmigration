fcf3b8935677d987f22cb59fd6bd522f021d33cb
Ruben Guardado <rguardado@viqsolutions.com>
2020-09-17 13:53:50 -0500

    addig conlumn

---
+++ TestEF.EntityFramework/Migrations/202009171848499_M4_adding_column.cs
@@ -0,0 +1,18 @@
+﻿namespace TestEF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class M4_adding_column : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Table1", "Prop3", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Table1", "Prop3");
+        }
+    }
+}
--- TestEF.EntityFramework/Models/Table1.cs
+++ TestEF.EntityFramework/Models/Table1.cs
@@ -12,5 +12,6 @@ namespace TestEF.Models
 
         public int Prop1 { get; set; }
         public int Prop2 { get; set; }
+        public string Prop3 { get; set; }
     }
 }
